8c7d39a0693d4f790f0ae199f57a7008f7d0f312
Florian Adonis <tanohzana@gmail.com>
2016-12-01 22:45:01 +0100

    creation pages et plugin Bootstrap

--- app/src/Controller/NdiController.php
+++ app/src/Controller/NdiController.php
@@ -26,7 +26,7 @@ use Cake\View\Exception\MissingTemplateException;
  *
  * @link http://book.cakephp.org/3.0/en/controllers/pages-controller.html
  */
-class PagesController extends AppController
+class NdiController extends AppController
 {
 
     /**
@@ -37,34 +37,16 @@ class PagesController extends AppController
      * @throws \Cake\Network\Exception\NotFoundException When the view file could not
      *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
-    public function display()
-    {
-        $path = func_get_args();
 
-        $count = count($path);
-        if (!$count) {
-            return $this->redirect('/');
-        }
-        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-            throw new ForbiddenException();
-        }
-        $page = $subpage = null;
+    public function index(){
 
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
+    }
 
-        try {
-            $this->render(implode('/', $path));
-        } catch (MissingTemplateException $e) {
-            if (Configure::read('debug')) {
-                throw $e;
-            }
-            throw new NotFoundException();
-        }
+    public function home(){
+
+    }
+
+    public function map(){
+
     }
 }
