bf8eeb06d66c7f273f6bbde01588b9fd5829ab82
Adrien Chardon <adrienchardon@mailoo.org>
2016-12-02 06:36:49 +0100

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,11 +13,14 @@ fi
 # getting the new code
 git clone https://github.com/iTeam-org/nuitinfo2k16.git
 
-# install deps
 cd nuitinfo2k16
-git checkout integration_view
+
+# add prod conf file
 cp ~/app.prod.php /var/www/ndi/nuitinfo2k16/app/config/app.default.php
+
 cd app
+
+# install deps
 composer install -n
 
 # To be used in config/app.php to disable debug mode
